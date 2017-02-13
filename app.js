4e8949547c94fb3e5f845b4244198250b2f7a2ce
Fazt <fabianncdev@gmail.com>
2017-02-13 03:04:06 -0500

    first app

---
+++ app.js
@@ -0,0 +1,78 @@
+'use strict';
+
+function MyApp() {
+  var version = 'v1.0',
+  appStorage = new AppStorage("taskAtHand");
+
+  function setStatus(message) {
+    $('#app>footer').text(message);
+  }
+
+  this.start = function () {
+    $('#new-task-name').keypress(function (e) {
+      if (e.which === 13) {
+        addTask();
+        return false;
+      }
+    }).focus();
+    $('#app>header').append(version);
+    setStatus('ready');
+  }
+}
+
+$(init);
+
+function init() {
+  window.app = new MyApp();
+  window.app.start();
+}
+
+function addTask() {
+  var taskName = $('#new-task-name').val();
+  if (taskName) {
+    addTaskElement(taskName);
+    $('#new-task-name').val('').focus();
+  }
+}
+
+function addTaskElement(taskName) {
+  //
+  var $task = $("#task-template .task").clone();
+  $("span.task-name", $task).text(taskName);
+  $("#task-list").append($task);
+  $("button.delete", $task).click(function() {
+    $task.remove();
+  });
+  $("button.move-up", $task).click(function() {
+    $task.insertBefore($task.prev());
+  });
+  $("button.move-down", $task).click(function() {
+    $task.insertAfter($task.next());
+  });
+  $('span.task-name', $task).click(function() {
+    onEditTaskName($(this));
+  });
+  $('input.task-name', $task).change(function () {
+    onChangeTaskname($(this))
+  }).blur(function () {
+    $(this).hide().siblings('span.task-name').show();
+  });
+  $('#task-list').append($task);
+}
+
+function onEditTaskName($span) {
+  $span.hide()
+    .siblings('input.task-name')
+    .val($span.text())
+    .show()
+    .focus();
+}
+
+function onChangeTaskname($input) {
+  $input.hide();
+  var $span = $input.siblings('span.task-name');
+  if ($input.val()) {
+    $span.text($input.val())
+  }
+  $span.show();
+}
